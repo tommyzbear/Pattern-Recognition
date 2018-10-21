04fae7ad8ed302ca54211d6d9e9532a6d2292269
tommyzbear <ajdsenwill@hotmail.com>
2018-10-21 14:39:02 +0100

    Updated Q1a with new data split method and added .gitignore file

--- CW1/CW1.py
+++ CW1/CW1.py
@@ -2,63 +2,91 @@ from mat4py import loadmat
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+# Loading face information in .mat data file
 data = loadmat('face(1).mat')
 
+# Data stored in key 'X' in the library with size 2576*520 and results stored in key 'l' with size 520*1
 faces = data.get("X")
 results = data.get("l")
 
 faces_arr = np.asarray(faces)
 results_arr = np.asarray(results)
 
+# Transpose into matrix with dimension 520*2576
 faces_arr = faces_arr.transpose()
 
+
+# Get resolutions of the images
 image_pixels = faces_arr.shape[-1]
 
-#print(faces_arr.shape, results_arr.shape)
+# Define train/test set ratio
+train_ratio = 0.9
+test_ratio = 0.1
 
-#print(type(faces_arr), type(results_arr))
+num_of_faces = faces_arr.shape[0]
 
-faces_train, faces_test, results_train, results_test = train_test_split(faces_arr, results_arr, test_size=0.3, random_state=10)
+num_of_train_faces = int(round(num_of_faces * train_ratio))
+num_of_test_faces = int(round(num_of_faces * test_ratio))
 
-#print(faces_train.shape, faces_test.shape)
+faces_train = np.zeros((num_of_train_faces, image_pixels))
+faces_test = np.zeros((num_of_test_faces, image_pixels))
+results_train = np.zeros(num_of_train_faces)
+results_test = np.zeros(num_of_test_faces)
 
+# Split training samples and test samples
+for i in range(0, int(round(num_of_faces / 10))):
+    start = i * 10
+    end = start + 10
+    single_face_arr = faces_arr[start: end]
+    single_face_result = results_arr[start: end]
+    faces_train_temp, faces_test_temp, results_train_temp, results_test_temp = train_test_split(single_face_arr, single_face_result, test_size=test_ratio, random_state=10)
+    start_train = start if start == 0 else start - i
+    end_train = end - i - 1
+    faces_train[start_train: end_train] = faces_train_temp
+    faces_test[i] = faces_test_temp
+    results_train[start_train: end_train] = results_train_temp
+    results_test[i] = results_test_temp
+
+# Initialize sum of training faces
 face_train_sum = np.zeros((1, image_pixels))
 
-faces_train_num = faces_train.shape[0]
-
-for face in range(faces_train_num):
+for face in range(num_of_train_faces):
     face_train_sum += faces_train[face]
 
-face_train_avg = face_train_sum / faces_train_num
+# Compute average face vector
+face_train_avg = face_train_sum / num_of_train_faces
 
-#print(face_avg)
+# Initialize covariance matrix
+faces_train_covariance = 1 / num_of_train_faces
 
-faces_train_covariance = 1 / faces_train_num
-
+# Normalized training faces by subtracting average face vector to each face
 normalized_faces_train = faces_train - face_train_avg
 
+# Compute covariance matrix
 faces_train_covariance *= np.matmul(normalized_faces_train.transpose(), normalized_faces_train)
 
-#print(faces_train_covariance.shape, '\n', faces_train_covariance)
-
+# Compute all eigen values and corresponding eigen vectors
 eigen_values, eigen_vectors = np.linalg.eig(faces_train_covariance)
 
-#print(eigen_values, "\n", eigen_vectors)
-
 # num of best eigen vectors
-M = 50
+M = 200
 
-largest_eigen_value_indices = np.argsort(eigen_values)[-50:]
+# Retrieve largest M eigen value indices in the array
+largest_eigen_value_indices = np.argsort(eigen_values)[-M:]
 
+# Initialize best eigen vectors
 best_eigen_vectors = np.zeros((M, image_pixels), dtype=np.complex)
 
+# Retrieve corresponding eigen vectors mapping to top M eigen values
 for i in range(0, M):
     best_eigen_vectors[i] = eigen_vectors[largest_eigen_value_indices[i]]
 
+# Compute projections of training faces onto eigen space
 projections_of_train_faces = np.matmul(normalized_faces_train, best_eigen_vectors.transpose())
 
-train_faces_reconstructed = np.zeros((faces_train_num, image_pixels))
+train_faces_reconstructed = np.zeros((num_of_train_faces, image_pixels))
 
+# Reconstruct training faces as linear combination of the best M eigen vectors
 for i in range(0, projections_of_train_faces.shape[-1]):
     linear_combination_of_eigen_vectors = np.zeros((1, image_pixels), dtype=np.complex)
     for j in range(0, M):
@@ -66,5 +94,3 @@ for i in range(0, projections_of_train_faces.shape[-1]):
         eigen_vector = best_eigen_vectors[j]
         linear_combination_of_eigen_vectors += [eig * projection for eig in eigen_vector]
     train_faces_reconstructed[i] = face_train_avg + linear_combination_of_eigen_vectors
-
-print(train_faces_reconstructed.shape)
